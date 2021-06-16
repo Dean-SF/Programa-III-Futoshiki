e1b259447a4fc62ade06ba48fc05812d78086589
Dean-SF <dsanabriaf@estudiantec.cr>
2021-06-15 20:37:27 -0600

    class "Configuracion" changed
    Se cambio la clase "Configuracion" por "Ventana_configuracion" y se termino de hacer la ventana, se agregaron un par de metodos para ayudar a hacer comprobaciones y algunas funcionalidades.
    Se volvio a crear la clase "Configuracion", esta mantiene la configuración de la ventana de configuracion

--- futoshiki.py
+++ futoshiki.py
@@ -9,21 +9,28 @@ class Generador():
 
     # Metodo para abrir la ventana de configuración
     def abrirConfiguracion(self):
-        self.ventana_configuracion = Configuracion(self.master)
+        self.ventana_configuracion = Ventana_configuracion(self.master)
         self.ventana_configuracion.grid(row=0,column=0,sticky="nswe")
     
     # Metodo para cerrar la ventana de configuración
     def cerrarConfiguracion(self):
         self.ventana_configuracion.destroy() # Con el metodo destroy() de tkinter se cierra
 
-# Clase contenedora de la ventana de configuracion
-class Configuracion(tk.Frame):
+# Clase que mantendra los datos de la configuración
+class Configuracion():
     dificultad = 0
+    reloj = 0
+    tiempo_reloj = (0,30,0)
+    posicion = 0
+
+# Clase contenedora de la ventana de configuracion
+class Ventana_configuracion(tk.Frame):
     def __init__(self,master):
         super().__init__(master)
 
+
         self.facil = tk.IntVar()
-        tk.Label(self,text="1. Nivel:").grid(row=0,column=0)
+        tk.Label(self,text="1. Nivel:").grid(row=0,column=0,sticky="w")
         tk.Checkbutton(self,text="Fácil",variable=self.facil,command=self.cambioDificultadFacil).grid(row=0,column=1,sticky="w")
 
         self.intermedio = tk.IntVar()
@@ -34,17 +41,63 @@ class Configuracion(tk.Frame):
 
         self.dificultadInicial()
 
+
+        tk.Label(self,text="1. Reloj:").grid(row=3,column=0,sticky="w")
+        self.reloj_si = tk.IntVar()
+        tk.Checkbutton(self,text="Si",variable=self.reloj_si,command=self.cambioRelojSi).grid(row=3,column=1,sticky="w")
+
+        self.reloj_no = tk.IntVar()
+        tk.Checkbutton(self,text="No",variable=self.reloj_no,command=self.cambioRelojNo).grid(row=4,column=1,sticky="w")
+
+        self.timer = tk.IntVar()
+        tk.Checkbutton(self,text="Timer",variable=self.timer,command=self.cambioRelojTimer).grid(row=5,column=1,sticky="w")
+
+        self.relojInicial()
+
+        verificacion_horas = self.register(self.comprobar_horas)
+        verificacion_minutos_segundos = self.register(self.comprobar_minutos_segundos)
+
+        self.hora = tk.StringVar()
+        tk.Label(self,text="Hora").grid(row=3,column=2,columnspan=2)
+        entry1 = tk.Entry(self,textvariable=self.hora,width=4)
+        entry1.grid(row=4,column=2,columnspan=2)
+        entry1.config(validate="key",validatecommand=(verificacion_horas,"%P"))
+
+        self.minutos = tk.StringVar()
+        tk.Label(self,text="Minutos").grid(row=3,column=3)
+        entry2 = tk.Entry(self,textvariable=self.minutos,width=6)
+        entry2.grid(row=4,column=3)
+        entry2.config(validate="key",validatecommand=(verificacion_minutos_segundos,"%P"))
+
+
+        self.segundos = tk.StringVar()
+        tk.Label(self,text="Segundos").grid(row=3,column=4)
+        entry3 = tk.Entry(self,textvariable=self.segundos,width=9)
+        entry3.grid(row=4,column=4)
+        entry3.config(validate="key",validatecommand=(verificacion_minutos_segundos,"%P"))
+
+        tk.Label(self,text="3. Posición en la ventana del panel de dígitos:").grid(row=6,column=0,columnspan=2)
+
+        self.derecha = tk.IntVar()
+        tk.Checkbutton(self,text="Derecha",variable=self.derecha,command=self.cambioDerecha).grid(row=6,column=2,sticky="w")
+
+        self.izquierda = tk.IntVar()
+        tk.Checkbutton(self,text="Izquierda",variable=self.izquierda,command=self.cambioIzquierda).grid(row=7,column=2,sticky="w")
+
+        self.posicionInicial()
+
         tk.Button(self,text="Aplicar",command=None).grid(row=8,column=0)
         tk.Button(self,text="Salir",command=lambda:WindowManager.cerrarConfiguracion()).grid(row=8,column=1)
 
-    # Metodo para colocar la dificultad que estaba elegida desde antes
+    # Metodo para colocar la dificultad que estaba en la configuración anterior
     def dificultadInicial(self):
-        if self.dificultad == 0:
+        if config.dificultad == 0:
             self.facil.set(1)
-        elif self.dificultad == 1:
+        elif config.dificultad == 1:
             self.intermedio.set(1)
-        elif self.dificultad == 2:
+        elif config.dificultad == 2:
             self.dificil.set(1)
+    
     # Metodos para que solo se pueda seleccionar una dificultad
     def cambioDificultadFacil(self):
         self.intermedio.set(0)
@@ -55,12 +108,75 @@ class Configuracion(tk.Frame):
     def cambioDificultadDificil(self):
         self.intermedio.set(0)
         self.facil.set(0)
-        
 
+    # Metodo para colocar el reloj que estaba en la configuración anterior
+    def relojInicial(self):
+        if config.reloj == 0:
+            self.reloj_si.set(1)
+        if config.reloj == 1:
+            self.reloj_no.set(1)
+        if config.reloj == 2:
+            self.timer.set(1)
+
+    # Metodos para que solo se pueda elegir un tipo de reloj
+    def cambioRelojSi(self):
+        self.reloj_no.set(0)
+        self.timer.set(0)
+    def cambioRelojNo(self):
+        self.reloj_si.set(0)
+        self.timer.set(0)
+    def cambioRelojTimer(self):
+        self.reloj_si.set(0)
+        self.reloj_no.set(0)
+    
+    # Metodo para colocar la posición que estaba en la configuración anterior
+    def posicionInicial(self):
+        if config.posicion == 0:
+            self.derecha.set(1)
+        elif config.posicion == 1:
+            self.izquierda.set(1)
+
+    # Metodos para que solo se pueda elegir una posición
+    def cambioDerecha(self):
+        self.izquierda.set(0)
+    def cambioIzquierda(self):
+        self.derecha.set(0)
+
+    # Metodo para comprobación de horas:
+    def comprobar_horas(self,entrada):
+        # Comprueba si la entrada es una de las 4 opciones presentadas
+        # para retornar True, de lo contrario retorna False
+        if entrada == "" or entrada == "2" or entrada == "1" or entrada == "0":
+            return True
+        return  False
+    
+    # Metodo para comprobación de minutos y segundos:
+    def comprobar_minutos_segundos(self,entrada):
+        # Si la entrada es un string vacio retorna True
+        if entrada == "":
+            return True
+
+        # Prueba a convertir un string en int
+        try:
+            
+            # Si logra convertir el string en int
+            entrada = int(entrada)
+
+            # Comprueba si es un numero entre 0 y 59
+            if 0 <= entrada <= 59:
+                return True
+            else:
+                return False
+
+        # Si no puede retorna False
+        except ValueError:
+            return False
 ################################################## Funciones ################################################
 
 
 ############################################### Programa principal ##########################################
+config = Configuracion()
+
 ventana = tk.Tk() # Crea ventana principal
 
 WindowManager = Generador(ventana) # Administra la creación de ventanas
