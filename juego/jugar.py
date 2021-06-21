f2d4a7eccc042e16c93ed215b9b52fcde3daebd8
Dean-SF <dsanabriaf@estudiantec.cr>
2021-06-21 11:33:52 -0600

    Panel development
    Se desarrollo el metodo "cargarNivel" el cual se podria cambiar el nombre en algun futuro, sirve para colocar un numero fijo en el tablero. Para su creacion se hizo una matriz con todos los botones en orden

--- juego/jugar.py
+++ juego/jugar.py
@@ -1,5 +1,6 @@
 ################################################## Imports ##################################################
 import tkinter as tk
+from typing import Collection
 from juego.config import config
 ################################################### Clases ##################################################
 class Panel(tk.Frame):
@@ -24,6 +25,9 @@ class Panel(tk.Frame):
         self.boton5 = tk.Button(self,text="",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton5))
         self.boton5.grid(row=0,column=8)
 
+        # Fila1 para una matriz de botones
+        fila1 = [self.boton1,self.boton2,self.boton3,self.boton4,self.boton5]
+
         # Separadores
         tk.Label(self,text="",font=mayor_menor_font).grid(row=1,column=0)
         tk.Label(self,text="",font=mayor_menor_font).grid(row=1,column=2)
@@ -46,6 +50,9 @@ class Panel(tk.Frame):
         tk.Label(self,text="",font=mayor_menor_font,width=2).grid(row=2,column=7)
         self.boton10 = tk.Button(self,text="",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton10))
         self.boton10.grid(row=2,column=8)
+        
+        # Fila2 para una matriz de botones
+        fila2 = [self.boton6,self.boton7,self.boton8,self.boton9,self.boton10]
 
         # Separadores
         tk.Label(self,text="",font=mayor_menor_font).grid(row=3,column=0)
@@ -55,20 +62,23 @@ class Panel(tk.Frame):
         tk.Label(self,text="",font=mayor_menor_font).grid(row=3,column=8)
 
         # tercer fila
-        self.boton11 = tk.Button(self,text="1",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton11))
+        self.boton11 = tk.Button(self,text="",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton11))
         self.boton11.grid(row=4,column=0)
         tk.Label(self,text="",font=mayor_menor_font,width=2).grid(row=4,column=1)
-        self.boton12 = tk.Button(self,text="1",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton12))
+        self.boton12 = tk.Button(self,text="",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton12))
         self.boton12.grid(row=4,column=2)
         tk.Label(self,text="",font=mayor_menor_font,width=2).grid(row=4,column=3)
-        self.boton13 = tk.Button(self,text="1",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton13))
+        self.boton13 = tk.Button(self,text="",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton13))
         self.boton13.grid(row=4,column=4)
         tk.Label(self,text="",font=mayor_menor_font,width=2).grid(row=4,column=5)
         self.boton14 = tk.Button(self,text="1",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton14))
         self.boton14.grid(row=4,column=6)
         tk.Label(self,text="",font=mayor_menor_font,width=2).grid(row=4,column=7)
-        self.boton15 = tk.Button(self,text="1",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton15))
+        self.boton15 = tk.Button(self,text="",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton15))
         self.boton15.grid(row=4,column=8)
+        
+        # Fila3 para una matriz de botones
+        fila3 = [self.boton11,self.boton12,self.boton13,self.boton14,self.boton15]
 
         # Separadores
         tk.Label(self,text="",font=mayor_menor_font).grid(row=5,column=0)
@@ -78,20 +88,23 @@ class Panel(tk.Frame):
         tk.Label(self,text="",font=mayor_menor_font).grid(row=5,column=8)
 
         # Cuarta fila
-        self.boton16 = tk.Button(self,text="1",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton16))
+        self.boton16 = tk.Button(self,text="",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton16))
         self.boton16.grid(row=6,column=0)
         tk.Label(self,text="",font=mayor_menor_font,width=2).grid(row=6,column=1)
-        self.boton17 = tk.Button(self,text="1",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton17))
+        self.boton17 = tk.Button(self,text="",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton17))
         self.boton17.grid(row=6,column=2)
         tk.Label(self,text="",font=mayor_menor_font,width=2).grid(row=6,column=3)
-        self.boton18 = tk.Button(self,text="1",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton18))
+        self.boton18 = tk.Button(self,text="",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton18))
         self.boton18.grid(row=6,column=4)
         tk.Label(self,text="",font=mayor_menor_font,width=2).grid(row=6,column=5)
-        self.boton19 = tk.Button(self,text="1",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton19))
+        self.boton19 = tk.Button(self,text="",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton19))
         self.boton19.grid(row=6,column=6)
         tk.Label(self,text="",font=mayor_menor_font,width=2).grid(row=6,column=7)
-        self.boton20 = tk.Button(self,text="1",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton20))
+        self.boton20 = tk.Button(self,text="",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton20))
         self.boton20.grid(row=6,column=8)
+
+        # Fila4 para una matriz de botones
+        fila4 = [self.boton16,self.boton17,self.boton18,self.boton19,self.boton20]
 
         # Separadores
         tk.Label(self,text="",font=mayor_menor_font).grid(row=7,column=0)
@@ -101,23 +114,35 @@ class Panel(tk.Frame):
         tk.Label(self,text="",font=mayor_menor_font).grid(row=7,column=8)
 
         # Quinta fila
-        self.boton21 = tk.Button(self,text="1",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton21))
+        self.boton21 = tk.Button(self,text="",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton21))
         self.boton21.grid(row=8,column=0)
         tk.Label(self,text="",font=mayor_menor_font,width=2).grid(row=8,column=1)
-        self.boton22 = tk.Button(self,text="1",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton22))
+        self.boton22 = tk.Button(self,text="",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton22))
         self.boton22.grid(row=8,column=2)
         tk.Label(self,text="",font=mayor_menor_font,width=2).grid(row=8,column=3)
-        self.boton23 = tk.Button(self,text="1",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton23))
+        self.boton23 = tk.Button(self,text="",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton23))
         self.boton23.grid(row=8,column=4)
         tk.Label(self,text="",font=mayor_menor_font,width=2).grid(row=8,column=5)
-        self.boton24 = tk.Button(self,text="1",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton24))
+        self.boton24 = tk.Button(self,text="",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton24))
         self.boton24.grid(row=8,column=6)
         tk.Label(self,text="",font=mayor_menor_font,width=2).grid(row=8,column=7)
-        self.boton25 = tk.Button(self,text="1",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton25))
+        self.boton25 = tk.Button(self,text="",font=font,width=3,height=1,command=lambda:self.cambioNumero(self.boton25))
         self.boton25.grid(row=8,column=8)
         
+        # Fila5 para una matriz de botones
+        fila5 = [self.boton21,self.boton22,self.boton23,self.boton24,self.boton25]
+
+        self.matriz_botones = [fila1,fila2,fila3,fila4,fila5]
+
     def cambioNumero(self,boton):
         boton["text"] = self.panel_digitos.digito
+    
+    # Metodo para cargar juego de la lista de partidas
+    def cargarNivel(self,coordenada,numero):
+        fila = coordenada[0]
+        columna = coordenada[1]
+        self.matriz_botones[fila][columna]["text"] = numero
+        self.matriz_botones[fila][columna]["state"] = tk.DISABLED
 
 class Digitos(tk.Frame):
     digito = 1
